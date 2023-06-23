e4f2b2b5dbbea90ab8aef3fbaaafcbc607862272
xgvr14 <andrei.gavriloiu@theaccessgroup.com>
2023-06-23 09:50:21 +0300

    Cerinta challengelui

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/*ask user a questin -> output three choices -> collect user input
+only one answer is correct - > provide appropiate message
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
