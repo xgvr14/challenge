0085c1023651ac6ef8ba4a4cb370b8e822e90c01
xgvr14 <andrei.gavriloiu@theaccessgroup.com>
2023-06-23 09:58:09 +0300

    import scanner + afisare intrebare si raspunsrui + user input

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 /*ask user a questin -> output three choices -> collect user input
 only one answer is correct - > provide appropiate message
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Do you know which of the answers bellow is a software testing method?");
+        System.out.println("A) yellow box");
+        System.out.println("B) boxing day");
+        System.out.println("C) black box");
+        String correctAnswer = "A)";
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please text the right answer A) B) or C) . Please keep the answer format : capsletter + ) with no space between");
+        String answer = userInput.nextLine();
+        System.out.println("Answer is " + answer);
+
+
+
+
+
+
         System.out.println("Hello world!");
     }
 }
