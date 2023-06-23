6515db09f0a2ad420cd198e49e111e4be767d8bd
xgvr14 <andrei.gavriloiu@theaccessgroup.com>
2023-06-23 09:55:38 +0300

    Stergere Hello World printer

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ only one answer is correct - > provide appropiate message
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
     }
 }
